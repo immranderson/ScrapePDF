86b79b513450bfdbc424d65716c696a060b24673
Tom <immranderson@gmail.com>
2013-04-03 14:35:10 -0700

    Include Needed Libraries

---
+++ src/com/tom/pdfscraper/RipByArea.java
@@ -0,0 +1,21 @@
+package com.tom.pdfscraper;
+
+import java.awt.Rectangle;
+import java.util.ArrayList;
+
+public class RipByArea {
+	
+	ArrayList<Rectangle> RecList;
+	
+	int upperLeftX, upperLeftY, lowerRightX, lowerRightY;
+	
+	public RipByArea()
+	{
+		
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/com/tom/pdfscraper/RipByText.java
@@ -0,0 +1,13 @@
+package com.tom.pdfscraper;
+
+public class RipByText {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
